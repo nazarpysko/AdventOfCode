b38cb05cc714870e26b51cdc6b8084adc7090f81
nazarpysko <nazarpysko27@gmail.com>
2022-09-23 10:44:04 +0200

    day 1_2 file added

--- 2015/day_01/day1_1.js
+++ 2015/day_01/day1_1.js
@@ -2,18 +2,13 @@ const fs = require('fs');
 
 const line = fs.readFileSync('input.txt', {encoding: 'utf-8'});
 
-let floor = 0, position, basementVisited = false;
+let floor = 0;
 
-line.split('').forEach((char, index)=> {
+line.split('').forEach((char)=> {
     if (char === '(')
         floor++;
     else
         floor--;
-
-    if (floor === -1 && !basementVisited) {
-        position = index + 1;
-        basementVisited = true;
-    } 
 })
 
-console.log(`Floors: ${floor} and first position to basement: ${position}`);
+console.log(`Floors: ${floor}`);

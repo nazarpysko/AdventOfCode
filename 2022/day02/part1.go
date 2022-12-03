2d0abf654155cc8e209478aed584e93b0acb2654
nazarpysko <nazarpysko27@gmail.com>
2022-12-03 17:17:09 +0100

    Day 2 part 2 done

--- 2022/day02/part1.go
+++ 2022/day02/part1.go
@@ -6,27 +6,6 @@ import (
 	"time"
 )
 
-const (
-	win      = 6
-	draw     = 3
-	lose     = 0
-	rock     = 1
-	paper    = 2
-	scissors = 3
-)
-
-var scorePlay = map[string]int{
-	"A X": draw + rock,
-	"A Y": win + paper,
-	"A Z": lose + scissors,
-	"B X": lose + rock,
-	"B Y": draw + paper,
-	"B Z": win + scissors,
-	"C X": win + rock,
-	"C Y": lose + paper,
-	"C Z": draw + scissors,
-}
-
 func rockScissorsPaper(input []string) {
 	totalScore := 0
 	for _, play := range input {
--- 2022/day02/part2.go
+++ 2022/day02/part2.go
@@ -6,9 +6,39 @@ import (
 	"time"
 )
 
-func test(input []string) {
+var playToWin = map[string]string{
+	"A": "Y",
+	"B": "Z",
+	"C": "X",
+}
+
+var playToLose = map[string]string{
+	"A": "Z",
+	"B": "X",
+	"C": "Y",
+}
+
+var playToDraw = map[string]string{
+	"A": "X",
+	"B": "Y",
+	"C": "Z",
+}
+
+func rockPaperScissorsSlave(input []string) {
 	totalScore := 0
 	for _, play := range input {
+
+		switch string(play[2]) {
+		case "X":
+			play = play[:2] + playToLose[string(play[0])]
+			break
+		case "Y":
+			play = play[:2] + playToDraw[string(play[0])]
+			break
+		case "Z":
+			play = play[:2] + playToWin[string(play[0])]
+		}
+
 		totalScore += scorePlay[play]
 	}
 
@@ -19,6 +49,6 @@ func main() {
 	input := utils.ReadInputFile(utils.GenInputFile(2))
 
 	timer := time.Now()
-	test(input)
+	rockPaperScissorsSlave(input)
 	fmt.Println("Time:", time.Since(timer))
 }

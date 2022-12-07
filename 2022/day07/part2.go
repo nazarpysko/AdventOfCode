f345113fc13d6eeef8d3d883499b4c6227763324
nazarpysko <nazarpysko27@gmail.com>
2022-12-07 10:44:04 +0100

    .gitignore added

---
+++ 2022/day07/part2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nazarpysko/AoC/2022/utils"
+	"time"
+)
+
+func solvePart2(input []string) {
+
+}
+
+func main() {
+	input := utils.ReadInputFile(utils.GenInputFile(07))
+
+	timer := time.Now()
+	solvePart2(input)
+	fmt.Println("Time:", time.Since(timer))
+}

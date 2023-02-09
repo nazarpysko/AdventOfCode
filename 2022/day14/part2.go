968e5fc0a6a148852ce091711edb4301b44956de
nazarpysko <nazarpysko27@gmail.com>
2023-02-09 10:27:29 +0100

    Debugging prints removed

--- 2022/day14/part2.go
+++ 2022/day14/part2.go
@@ -7,7 +7,25 @@ import (
 )
 
 func solvePart2(input []string) {
+	// Make the cave
+	cave := (&Cave{}).newCave()
 
+	for _, path := range input {
+		cave.addPath(path)
+	}
+
+	sandGrains := 0
+
+	// Pour the sand until source is sand
+	for {
+		if !cave.pourSand() {
+			break
+		}
+
+		sandGrains += 1
+	}
+
+	fmt.Println("Total units of sand until reach the abyss:", sandGrains)
 }
 
 func main() {
